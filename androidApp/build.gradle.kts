29fd8592727284cfc913c9be01b0d130896fbbf3
Mari Celmer <marivane.slod@gmail.com>
2026-08-21 16:56:00 -0300

    Bump app version to 26.1.1 and set NDK debug symbol level to FULL

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -104,6 +104,7 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
+            ndk.debugSymbolLevel = "FULL"
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
